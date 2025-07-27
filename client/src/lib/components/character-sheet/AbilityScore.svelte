4605ea1bfa86c8ff0083e1c1fe47c92bd9d48f76
Juan Pablo Gonzalez <59584304+juanpablopiano@users.noreply.github.com>
2025-07-27 00:34:17 -0400

    Character sheet page (#12)
    * chore: add character sheet page on client
    
    * feat: base styles (#10)
    
    * chore: add new sections to character sheet
    
    ---------
    
    Co-authored-by: Carlos Matos <80372027+lematosdev@users.noreply.github.com>

---
+++ client/src/lib/components/character-sheet/AbilityScore.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { getAbilityModifier, getPlusSign } from '$lib/utils';
+
+  interface Props {
+    label: string;
+    value: number;
+  }
+  let { label, value }: Props = $props();
+</script>
+
+<div class="w-full h-34 border-3 bg-white">
+  <p class="uppercase text-center text-sm font-bold">{label}</p>
+  <p class="grid place-items-center h-22">
+    <span class="text-4xl font-"
+      >{getPlusSign(value)}{getAbilityModifier(value)}</span
+    >
+  </p>
+  <div class="w-full flex justify-center">
+    <input
+      type="text"
+      class="rounded-full w-14 h-8 text-center border-3"
+      bind:value
+      aria-label="{label} score"
+    />
+  </div>
+</div>
