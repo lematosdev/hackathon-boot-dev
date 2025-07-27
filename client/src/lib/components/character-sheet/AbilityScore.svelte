5799c8942d1110de65c9d34c3fd653536dd3956b
Carlos Matos <lematosdev@gmail.com>
2025-07-27 15:57:40 -0400

    style: add svg

--- client/src/lib/components/character-sheet/AbilityScore.svelte
+++ client/src/lib/components/character-sheet/AbilityScore.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-  import { currentCharacter, saveCurrent } from '$lib/stores/characters';
-  import { getAbilityModifier, getPlusSign } from '$lib/utils';
+	import { currentCharacter, saveCurrent } from "$lib/stores/characters";
+	import { getAbilityModifier, getPlusSign } from "$lib/utils";
 
-  interface Props {
-    label: string;
-    value: number;
-  }
-  let { label, value }: Props = $props();
+	interface Props {
+		label: string;
+		value: number;
+	}
+	let { label, value }: Props = $props();
 </script>
 
 <div class="w-full h-34 border-3">
-  <p class="uppercase text-center text-sm font-bold">{label}</p>
-  <p class="grid place-items-center h-22">
-    <span class="text-4xl font-">{getPlusSign(value)}{
-      getAbilityModifier(value)
-    }</span>
-  </p>
-  <div class="w-full flex justify-center">
-    <input
-      type="text"
-      class="rounded-full w-14 h-8 text-center border-3 bg-white! text-black!"
-      bind:value
-      oninput={() =>
-        saveCurrent({
-          attributes: {
-            ...$currentCharacter.attributes,
-            [label.toLowerCase()]: Number(value) || 0,
-          },
-        })}
-      aria-label="{label} score"
-    />
-  </div>
+	<p class="uppercase text-center text-sm font-bold">{label}</p>
+	<p class="grid place-items-center h-22">
+		<span class="text-4xl font-"
+			>{getPlusSign(value)}{getAbilityModifier(value)}</span
+		>
+	</p>
+	<div class="w-full flex justify-center">
+		<input
+			type="text"
+			class="rounded-full w-14 h-8 text-center border-3 bg-white! text-black!"
+			bind:value
+			oninput={() =>
+				saveCurrent({
+					attributes: {
+						...$currentCharacter.attributes,
+						[label.toLowerCase()]: Number(value) || 0
+					}
+				})}
+			aria-label="{label} score"
+		/>
+	</div>
 </div>
--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,42 +1,36 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import CharacterCard from '$lib/components/character-selection/CharacterCard.svelte';
-  import NewCharacterCard from '$lib/components/character-selection/NewCharacterCard.svelte';
-  import type { CharacterSheet } from '../../../types/characterSheet';
-  import {
-    elenorSwiftleaf,
-    thorgarIronfist,
-  } from '$lib/utils/testcharacters';
-  import { saveTestCharacters } from '$lib/utils';
-  import { characterList } from '$lib/stores/characters';
+	import { onMount } from "svelte";
+	import CharacterCard from "$lib/components/character-selection/CharacterCard.svelte";
+	import NewCharacterCard from "$lib/components/character-selection/NewCharacterCard.svelte";
+	import type { CharacterSheet } from "../../../types/characterSheet";
+	import { elenorSwiftleaf, thorgarIronfist } from "$lib/utils/testcharacters";
+	import { saveTestCharacters } from "$lib/utils";
+	import { characterList } from "$lib/stores/characters";
 
-  // Example data
-  const testCharacters: CharacterSheet[] = [
-    elenorSwiftleaf,
-    thorgarIronfist,
-  ];
+	// Example data
+	const testCharacters: CharacterSheet[] = [elenorSwiftleaf, thorgarIronfist];
 
-  onMount(() => {
-    if (!localStorage.getItem('characters')) {
-      saveTestCharacters(testCharacters);
-    }
-  });
+	onMount(() => {
+		if (!localStorage.getItem("characters")) {
+			saveTestCharacters(testCharacters);
+		}
+	});
 </script>
 
 <div class="max-w-8xl m-auto p-2 sm:p-8">
-  <div class="mb-8 flex flex-row justify-center">
-    <h1 class="text-2xl font-bold mb-4">Created Characters</h1>
-  </div>
+	<div class="mb-8 flex flex-row justify-center">
+		<h1 class="text-2xl font-bold mb-4">Created Characters</h1>
+	</div>
 
-  <div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-fr">
-    {#each $characterList ?? [] as char (char.characterName)}
-      <CharacterCard
-        charName={char.characterName}
-        charClass={char.class}
-        level={char.level}
-        imageUrl={''}
-      />
-    {/each}
-    <NewCharacterCard />
-  </div>
+	<div class="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-fr">
+		{#each $characterList ?? [] as char (char.characterName)}
+			<CharacterCard
+				charName={char.characterName}
+				charClass={char.class}
+				level={char.level}
+				imageUrl={""}
+			/>
+		{/each}
+		<NewCharacterCard />
+	</div>
 </div>
