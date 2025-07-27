233c49912abf650a7ff0f32412b5575ee933a0a5
Carlos Matos <lematosdev@gmail.com>
2025-07-27 15:57:40 -0400

    style: add svg

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from "node:path";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: {
+		adapter: adapter(),
+		alias: {
+			"@types": path.resolve("../types/dnd/index.ts")
+		}
+
+	}
 };
 
 export default config;
