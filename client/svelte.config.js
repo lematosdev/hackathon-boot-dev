7b3e9fdf66b60f15d8224c8e16a318791e5f5c11
Juan Pablo Gonzalez <59584304+juanpablopiano@users.noreply.github.com>
2025-07-28 09:42:08 -0400

    deploy: generate deploy config (#25)

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,19 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from 'svelte-adapter-deno';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import path from "node:path";
+import path from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		alias: {
-			"@types": path.resolve("../types/dnd/index.ts")
-		}
-
-	}
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+    alias: {
+      '@types': path.resolve('../types/dnd/index.ts'),
+    },
+  },
 };
 
 export default config;
